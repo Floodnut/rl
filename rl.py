77990504cf7a053cb021d3f811b29c070d168ce7
Kuemjong <gsniper777@gmail.com>
2023-10-24 21:49:12 +0900

    feat: algorithms

--- rl.py
+++ rl.py
@@ -3,34 +3,33 @@ import random
 
 
 class Greedy:
-    def __init__(self):
-        pass
-
-    def action(self, a: int, b: int) -> int:
-        return (0, a) if a > b else (1, b)
+    # arm의 가치
+    def action(self, q_values: list) -> int:
+        return np.argmax(q_values)
 
 
 class E_Greedy:
-    def __init__(self):
-        pass
+    # 0~1 사이의 epsilon
+    def __init__(self, e):
+        self.e = e
 
-    def action(self, q_values: list, epsilon: float) -> int:
-        if random.random() < epsilon:
+    # arm의 가치
+    def action(self, q_values: list) -> int:
+        if random.random() < self.e:
             return random.randint(0, len(q_values) - 1)
         else:
             return np.argmax(q_values)
 
 
 class UCB:
-    def __init__(self):
-        pass
+    # exploration과 exploitation 조절
+    def __init__(self, c):
+        self.c = c
 
-    def action(self, q_values: list, n_plays: list, c: float) -> int:
-        total_plays = sum(n_plays)
-        ucb_values = q_values + c * np.sqrt(
-            np.log(total_plays) / (np.array(n_plays) + 1)
+    def action(self, q_values: list, n_plays: list, total_plays: int) -> int:
+        ucb_values = q_values + self.c * np.sqrt(
+            np.log(total_plays) / (np.array(n_plays) + 0.000001)
         )
-
         return np.argmax(ucb_values)
 
 
@@ -38,6 +37,7 @@ class ThompsonSampling:
     def __init__(self):
         pass
 
+    # 성공, 실패 횟수 (베타 분포)
     def action(self, alpha: list, beta: list) -> int:
         samples = [np.random.beta(a, b) for a, b in zip(alpha, beta)]
         return np.argmax(samples)
