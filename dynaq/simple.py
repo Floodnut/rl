a6756572eb9af408dba8b07876e6f34689263ce9
Floodnut <gsniper777@gmail.com>
2023-12-20 21:13:31 +0900

    feat: done

--- dynaq/simple.py
+++ dynaq/simple.py
@@ -42,16 +42,13 @@ class MazeRunner(Maze):
     """
     def __init__(self, limit: int, model: dict, q: dict):
         super().__init__()
-        self.cur_state = [self.start[0], self.start[1]]
         self.limit = limit
         
         self.model = model
         self.q_values = q
         
-        self.reward = 0
-        
         # for e-greedy
-        self.epsilon = 0.05
+        self.epsilon = 0.1
         
     def reset(self):
         pass
@@ -66,21 +63,24 @@ class MazeRunner(Maze):
         """get max value from q_values
         """
 
-        return max([self.q_values[s][a] for a in range(4)])      
+        max_result = max([self.q_values[s][a] for a in range(4)])
+        return max_result
 
 
-    def e_greedy(self, state) -> int:
+    def e_greedy(self, s) -> int:
         """(b) a <- epsilon-greedy(s, Q)
         
         Choose action via epsilon-greedy
         """
+
         if random.random() < self.epsilon:
             return random.randint(0, 3)
         else:
-            return max([0, 1, 2, 3], key=self.q_values[s].get)
+            k = max([0, 1, 2, 3], key=self.q_values[s].get)
+            return k
 
          
-    def do_action(self, action: int) -> Tuple[int]:
+    def do_action(self, s: tuple, action: int) -> Tuple[int]:
         """(c) Execute action a;
         
         Do action to update current state (move state)
@@ -88,30 +88,30 @@ class MazeRunner(Maze):
         """
         d = self.actions[action]
         
-        if self._is_valid_pos(self.cur_state[0] + d[0], self.cur_state[1] + d[1]):
+        if self._is_valid_pos(s[0] + d[0], s[1] + d[1]):
             return (d[0], d[1])
         
         return (0, 0)
 
-    def observe(self, movement: Tuple[int]) -> tuple:
+    def observe(self, s: tuple, movement: Tuple[int]) -> tuple:
         """(c) observe resultant reward, r, and state, s'
         
         Return next state (s') and reward (r) in tuple
         """
         
-        s_prime = (self.cur_state[0] + movement[0], self.cur_state[1] + movement[1])
+        s_prime = (s[0] + movement[0], s[1] + movement[1])
         r = -1 if s_prime != self.goal else 0
         
         return s_prime, r
     
-    def q_learning(self, state: tuple, action: int, r: int):
+    def q_learning(self, state: tuple, state_prime: tuple, action: int, r: int):
         """(d) Q(s, a) <- Q(s, a) + alpha[r + gamma * max_a' Q(s', a') - Q(s, a)]
         
         Value iteration to get optimal policy
         """
         
         self.q_values[state][action] += self.alpha * (
-            r + self.gamma * self.get_max(state) - self.q_values[state][action]
+            r + self.gamma * self.get_max(state_prime) - self.q_values[state][action]
         )
 
     def model_update(self, s: tuple, a: int, s_prime: tuple, r: int):
@@ -119,9 +119,14 @@ class MazeRunner(Maze):
         
         Assume deterministic environment
         """
+        if s not in self.model:
+            self.model[s] = {}
+        
         self.model[s][a] = (s_prime, r)
 
-    def dyna_q(self):
+    def dyna_q(self) -> int:
+        step = 0
+        
         # (a) s <- current (nonterminal) state
         s = self.start
         while True:
@@ -129,9 +134,10 @@ class MazeRunner(Maze):
             a = self.e_greedy(s)
             
             # (c) Execute action a; observe resultant state, s', and reward, r 
-            movement = self.do_action(a)
-            s_p, r = self.observe(movement)
-            self.q_learning(s, a, r)
+            movement = self.do_action(s, a)
+            s_p, r = self.observe(s, movement)
+            
+            self.q_learning(s, s_p, a, r)
             
             # (e) Model(s, a) <- s', r
             self.model_update(s, a, s_p, r)
@@ -140,46 +146,61 @@ class MazeRunner(Maze):
             repeat = 0
             while (repeat < self.limit):
                 # s <- random previously observed state
-                s = random.choice(self.model.keys())
+                _s = random.choice(list(self.model.keys()))
                 
                 # a <- random action previously taken in s
-                a = random.choice([0, 1, 2, 3])
+                _a = random.choice(list(self.model[_s].keys()))
                 
-                # s', r <- Model(s, a)
-                s_p, r = self.model[s][a]
+                # s', r <- Model(s, a)                
+                _s_p, _r = self.model[_s][_a]
                 
                 # Q(s, a) <- Q(s, a) + alpha[r + gamma * max_a' Q(s', a') - Q(s, a)]
-                q_values[s][a] += self.alpha * (
-                    r + self.gamma * self.get_max(s) - q_values[s][a]
+                q_values[_s][_a] += self.alpha * (
+                    _r + self.gamma * self.get_max(_s_p) - q_values[_s][_a]
                 )
-                
+
                 repeat += 1
-                
-            print(repeat, s_p)
+
             if s_p == self.goal:
-                print("Goal!")
+                # print("Goal!")
                 break
-            
+
+            # print(s, s_p)
             s = s_p
+            step += 1
+        
+        return step
                 
     def result(self):
         pass
     
 if __name__ == "__main__":
-    for planning_step in [0, 5, 50]:
-        # Initialize Q(s, a) and Model(s, a) for all s ∈ S, a ∈ A(s)
-        models: dict = {}
-        q_values: dict = {}
-        
-        for w, h in product(range(9), range(6)):            
-            s = (w, h)
-            models[s] = {}
-            q_values[s] = {}
-            
-            for a in range(4):
-                q_values[s][a] = random.random() / 100
+    steps_per_episode = {
+        0: [],
+        50: [],
+        500: [],
+    }
+    
+    for planning_step in [0, 50, 500]:
+        print(f"Planning step: {planning_step}")
 
-        maze_runner = MazeRunner(limit=planning_step, model=models, q=q_values)
-        maze_runner.dyna_q()
+        step = 0
+        for episode in range(1000):
+            print(f"Episode: {episode}")        
+            # Initialize Q(s, a) and Model(s, a) for all s ∈ S, a ∈ A(s)
+            models: dict = {}
+            q_values: dict = {}
+            
+            for h, w in product(range(6), range(9)):            
+                s = (h, w)
+                q_values[s] = {}
+                
+                for a in range(4):
+                    q_values[s][a] = random.random() / 100
+
+            maze_runner = MazeRunner(limit=planning_step, model=models, q=q_values)
+            step = maze_runner.dyna_q()
+            
+        steps_per_episode[planning_step].append(step)
         
-        maze_runner.result()
+        print(steps_per_episode)
